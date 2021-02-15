d84bfb9716b88d093a5275c93b6d612a62c1efca
Christian Nagel <christian@christiannagel.com>
2021-02-15 13:03:23 +0100

    managed identity

---
+++ 03_AzureAppConfigurationWithIdentity/WebAppForAzureAppConfig/Program.cs
@@ -0,0 +1,35 @@
+using Azure.Identity;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace WebAppForAzureAppConfig
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults((webBuilder) =>
+                {
+                    webBuilder.ConfigureAppConfiguration(config =>
+                    {
+                        DefaultAzureCredential credential = new(includeInteractiveCredentials: true);
+                        var settings = config.Build();
+                        // read from user secrets
+                        string azureAppConfigurationEndpoint = settings["AzureAppConfigurationEndpoint"];
+
+                        config.AddAzureAppConfiguration(azureAppConfig =>
+                        {
+                            azureAppConfig.Connect(new Uri(azureAppConfigurationEndpoint), credential);
+                        });
+                    });
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
