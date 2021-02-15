3fee5fe57a8260943a3bff11949278b6b11172b9
Christian Nagel <christian@christiannagel.com>
2021-02-15 14:38:21 +0100

    feature flags

---
+++ appconfig/05_FeatureFlags/WebAppForAzureAppConfig/Program.cs
@@ -0,0 +1,44 @@
+using Azure.Identity;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace WebAppForAzureAppConfig
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults((webBuilder) =>
+                {
+                    webBuilder.ConfigureAppConfiguration((context, config) =>
+                    {
+                        DefaultAzureCredential credential = new(includeInteractiveCredentials: true);
+                        var settings = config.Build();
+                        // read from user secrets
+                        string azureAppConfigurationEndpoint = settings["AzureAppConfigurationEndpoint"];
+
+                        config.AddAzureAppConfiguration(azureAppConfig =>
+                        {
+                            azureAppConfig.Connect(new Uri(azureAppConfigurationEndpoint), credential)
+                            .Select(KeyFilter.Any, LabelFilter.Null)
+                            .Select(KeyFilter.Any, context.HostingEnvironment.EnvironmentName)
+                            .ConfigureRefresh(refresh =>
+                            {
+                                refresh.Register("AppConfigurationSolutionSample.Sentinel",
+                                    refreshAll: true)
+                                .SetCacheExpiration(TimeSpan.FromMinutes(5));
+                            }).UseFeatureFlags();
+                        });
+                    });
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
